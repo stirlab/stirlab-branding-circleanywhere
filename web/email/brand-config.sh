d19fc69db98d8be5edf62b047c9d2510003c8b77
Chad Phillips <chad@apartmentlines.com>
2017-05-04 11:35:31 -0700

    branded email templates

---
+++ web/email/brand-config.sh
@@ -0,0 +1,8 @@
+name="CircleAnywhere"
+domain="circleanywhere.com"
+base_url="https://connect.circleanywhere.com"
+email="circleanywhere.help@gmail.com"
+
+background="#FFFFFF"
+accent="#800000"
+accent_dark="#600000"
