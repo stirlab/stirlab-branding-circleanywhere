ad19a2fe15c512565439a124a292c57f4f069520
Chad Phillips <chad@apartmentlines.com>
2017-05-04 17:26:06 -0700

    add video config and images

---
+++ video/site.js
@@ -0,0 +1,11 @@
+var config = {};
+
+config.video_mute_image = 'circleanywhere-conference-video-mute.png';
+config.video_no_video_avatar_image = 'circleanywhere-conference-video-no-video-avatar.png';
+config.video_banner_text_color = '#FFFFFF';
+config.video_banner_background = '#800000';
+config.video_mute_banner_text_color = '#800000';
+config.video_mute_banner_background = '#FFFFFF';
+
+module.exports.config = config;
+
