f66ff517bf48bff3d382fe650aed39c57d3a89be
Chad Phillips <chad@apartmentlines.com>
2017-05-19 19:07:38 -0700

    switch name banner background to black

--- video/site.js
+++ video/site.js
@@ -3,7 +3,7 @@ var config = {};
 config.video_mute_image = 'circleanywhere-conference-video-mute.png';
 config.video_no_video_avatar_image = 'circleanywhere-conference-video-no-video-avatar.png';
 config.video_banner_text_color = '#FFFFFF';
-config.video_banner_background = '#800000';
+config.video_banner_background = '#000000';
 config.video_mute_banner_text_color = '#800000';
 config.video_mute_banner_background = '#FFFFFF';
 
